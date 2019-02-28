e8ad26cb8f333bbbc47f0bc8559446edf0e6384e
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-02-28 17:07:47 -0500

    added basic action to add new movie

--- client/src/Movies/MovieCreate.js
+++ client/src/Movies/MovieCreate.js
@@ -1,14 +1,56 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import {withRouter} from 'react-router-dom';
 
 const MovieCreate = props => {
 	const [movie, setMovie] = useState({title: '', director: '', metascore: 0, stars: []});
 
 	return (
-		<div>
-			<h1>OK</h1>
-		</div>
+		<form onSubmit={(e) => {
+			e.preventDefault();
+			axios
+				.post('http://localhost:5000/api/movies', movie)
+				.then(response => {
+					props.history.push('/');
+				})
+				.catch(error => {
+					console.error('Server Error', error);
+				});
+		}}>
+			<input 
+				type="text"
+				name="title"
+				onChange={(e) => setMovie({...movie, title: e.target.value})}
+				value={movie.title}
+			/>
+			
+			<input
+				type="text"
+				name="director"
+				onChange={(e) => setMovie({...movie, director: e.target.value})}
+				value={movie.director}
+			/>
+			
+			<input
+				type="number"
+				name="metascore"
+				onChange={(e) => setMovie({...movie, metascore: e.target.value})}
+				value={movie.metascore}
+			/>
+
+			<input
+				type="text"
+				name="stars"
+				onChange={(e) => setMovie({...movie, stars: e.target.value.split(",")})}
+				value={movie.stars}
+			/>
+
+			<input
+				type="submit"
+				value="Add new movie"
+			/>
+		</form>
 	);
 }
 
-export default MovieCreate;
+export default withRouter(MovieCreate);
