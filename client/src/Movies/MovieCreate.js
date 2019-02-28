54492d80df5515ef20ccd5c7e42b7e83c913f770
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-02-28 17:25:02 -0500

    added fully working form to create movies & its styles

--- client/src/Movies/MovieCreate.js
+++ client/src/Movies/MovieCreate.js
@@ -6,50 +6,60 @@ const MovieCreate = props => {
 	const [movie, setMovie] = useState({title: '', director: '', metascore: 0, stars: []});
 
 	return (
-		<form onSubmit={(e) => {
-			e.preventDefault();
-			axios
-				.post('http://localhost:5000/api/movies', movie)
-				.then(response => {
-					props.history.push('/');
-				})
-				.catch(error => {
-					console.error('Server Error', error);
-				});
-		}}>
-			<input 
-				type="text"
-				name="title"
-				onChange={(e) => setMovie({...movie, title: e.target.value})}
-				value={movie.title}
-			/>
-			
-			<input
-				type="text"
-				name="director"
-				onChange={(e) => setMovie({...movie, director: e.target.value})}
-				value={movie.director}
-			/>
-			
-			<input
-				type="number"
-				name="metascore"
-				onChange={(e) => setMovie({...movie, metascore: e.target.value})}
-				value={movie.metascore}
-			/>
+		<div className="save-wrapper">
+			<form
+				className="movie-card"
+				onSubmit={(e) => {
+				e.preventDefault();
+				axios
+					.post('http://localhost:5000/api/movies', movie)
+					.then(response => {
+						props.history.push('/');
+					})
+					.catch(error => {
+						console.error('Server Error', error);
+					});
+			}}>
+				<h2 className="card-title">Add new movie</h2>
 
-			<input
-				type="text"
-				name="stars"
-				onChange={(e) => setMovie({...movie, stars: e.target.value.split(",")})}
-				value={movie.stars}
-			/>
+				<h4>Title:</h4>
+				<input 
+					type="text"
+					name="title"
+					onChange={(e) => setMovie({...movie, title: e.target.value})}
+					value={movie.title}
+				/>
+				
+				<h4>Director:</h4>
+				<input
+					type="text"
+					name="director"
+					onChange={(e) => setMovie({...movie, director: e.target.value})}
+					value={movie.director}
+				/>
+				
+				<h4>Metascore:</h4>
+				<input
+					type="number"
+					name="metascore"
+					onChange={(e) => setMovie({...movie, metascore: e.target.value})}
+					value={movie.metascore}
+				/>
+				<h4>Stars:</h4>
+				<p>Use comma (,) to separate the people.</p>
+				<input
+					type="text"
+					name="stars"
+					onChange={(e) => setMovie({...movie, stars: e.target.value.split(",")})}
+					value={movie.stars}
+				/>
 
-			<input
-				type="submit"
-				value="Add new movie"
-			/>
-		</form>
+				<input
+					type="submit"
+					value="Add new movie"
+				/>
+			</form>
+		</div>
 	);
 }
 
